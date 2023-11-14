ee0e29a930fabe3bf22b032dd7443ca47fea5513
Carlos Ocampo <108969977+Carlex27@users.noreply.github.com>
2023-11-14 10:00:38 -0600

    ordenamiento

--- src/MetodosOrdenamiento/Burbuja.java
+++ src/MetodosOrdenamiento/Burbuja.java
@@ -6,6 +6,8 @@ public class Burbuja {
         int[] arreglo = {5, 4, 3, 2, 1};
         int auxiliar;
         boolean cambios = false;
+        long inicio, fin;
+        inicio = System.currentTimeMillis();
         while (true) {
             cambios = false;
             for (int i = 1; i < arreglo.length; i++) {
@@ -20,8 +22,11 @@ public class Burbuja {
                 break;
             }
         }
+        fin = System.currentTimeMillis();
         for (int i = 0; i < arreglo.length; i++) {
             System.out.println(arreglo[i]);
         }
+        System.out.println((float)(fin - inicio)/1000 + " segundos");
+        
     }
 }
---
+++ src/MetodosOrdenamiento/Radix.java
@@ -0,0 +1,8 @@
+package MetodosOrdenamiento;
+//Clase que implementa el metodo de ordenamiento Radix
+public class Radix {
+    //Metodo main
+    public static void main(String[] args) {
+        
+    }
+}
